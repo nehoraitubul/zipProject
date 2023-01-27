4d0c22d9fd6b6dc8be2296e6bd9b2e4ab4c1f51f
nehoraitubul <nehorai0258@gmail.com>
2023-01-27 11:49:06 +0200

    regular commit

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
 import main_class, zip_class, os, re
 
-def check_path(paths):
-    is_dir_list = []
-    for my_path in paths:
-        is_dir_list.append(os.path.isdir(my_path))
-    # return os.path.exists(my_path), is_dir_list
-    return True, is_dir_list
+def check_path(my_path):
+    return os.path.exists(my_path)
+
 
 if __name__ == '__main__':
     i = zip_class.Zip
-    path = "C:\ZipProjectTests\Trans_ebays","C:\ZipProjectTests\\new_append.txt"
+    # path = "C:\ZipProjectTests\Trans_ebays","C:\ZipProjectTests\\new_append.txt"
+    path = "C:\ZipProjectTests\Trans_ebays"
 
-    # path_exists, is_dir_list = check_path(path)
-    # if path_exists:
-    #     i.zip_dir(path, compress=True, compress_level=1, _is_dir=is_dir_list)
+    path_exists = check_path(path)
+    if path_exists:
+        i.zip_dir(path, compress=True, compress_level=1)
--- zip_class.py
+++ zip_class.py
@@ -3,13 +3,13 @@ import main_class
 
 class Zip(main_class.Main):
 
-    def zip_dir(*args, compress=False, compress_level=None, store_path='default', _is_dir):
+    def zip_dir(*args, compress=False, compress_level=None, store_path='default'):
         print(args)
 
         compress = "zipfile.ZIP_DEFLATED" if compress else "zipfile.ZIP_STORED"
 
-        for integer, path in enumerate(args):
-            if _is_dir[integer]:
+        for path in args:
+            if os.path.isdir(path):
 
                 with zipfile.ZipFile("%s.zip" % path, 'w', compression=zipfile.ZIP_DEFLATED, compresslevel=compress_level) as zip:
                     for root, directories, files in os.walk(path):
